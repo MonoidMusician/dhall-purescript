937b2029e2fdd0d07412363bda1b0aae2311f1dc
Nicholas Scheel <monoidmusician@gmail.com>
2018-09-30 12:03:46 -0400

    Styling updates, now featuring autofocusing autosized text inputs

--- src/Dhall/Interactive/Halogen/Icons.purs
+++ src/Dhall/Interactive/Halogen/Icons.purs
@@ -2,18 +2,22 @@ module Dhall.Interactive.Halogen.Icons where
 
 import Prelude
 
+import Control.Bind (bindFlipped)
+import Control.Monad.Free (Free)
 import DOM.HTML.Indexed (HTMLbutton)
 import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..), isNothing)
 import Data.Profunctor.Star (Star(..))
+import Data.Symbol (SProxy(..))
 import Data.Tuple (Tuple(..))
-import Dhall.Interactive.Halogen.Types (simpleC)
 import Effect (Effect)
-import Effect.Aff (Aff)
+import Effect.Aff (Aff, error, throwError)
+import Effect.Class.Console (logShow)
 import Halogen (AttrName(..), ClassName(..), ElemName(..), Namespace(..))
 import Halogen as H
 import Halogen.Aff as HA
+import Halogen.Expanding as TCHE
 import Halogen.HTML as HH
 import Halogen.HTML.Elements.Keyed as HK
 import Halogen.HTML.Events as HE
@@ -24,6 +28,7 @@ import Halogen.Zuruzuru as Zuruzuru
 import Web.UIEvent.KeyboardEvent (KeyboardEvent)
 import Web.UIEvent.MouseEvent (MouseEvent)
 import Web.UIEvent.WheelEvent (WheelEvent)
+import Web.Util.TextCursor as TC
 
 svgNS = Namespace "http://www.w3.org/2000/svg" :: Namespace
 xlinkNS = Namespace "http://www.w3.org/1999/xlink" :: Namespace
@@ -146,6 +151,55 @@ data Q a
   = ZZ (Zuruzuru.Message String) a
   | Swap a
 
+settings :: TCHE.Settings
+settings =
+  { focused:
+    { min: 100.0
+    , max: 600.0
+    , padding: 50.0
+    }
+  , blurred:
+    { min: 100.0
+    , max: 600.0
+    , padding: 50.0
+    }
+  }
+
+data QE a
+  = Down String a
+  | Up TCHE.Blurry a
+  | PassOn (Free TCHE.Query a)
+
+expanding :: H.Component HH.HTML QE String String Aff
+expanding = H.component
+  { initialState: TCHE.Blurred
+  , initializer: Nothing
+  , finalizer: Nothing
+  , receiver: HE.input Down
+  , eval: case _ of
+      Down new next -> next <$ do
+        old <- H.get
+        when (new /= TCHE.toString old) do
+          H.put (TCHE.Blurred new)
+      Up new next -> next <$ do
+        old <- H.get
+        when (new /= old) do
+          H.put new
+          H.raise (TCHE.toString new)
+      -- Let the caller query the expanding component directly
+      -- But make sure we grab the new state so we are not surprised
+      PassOn q -> H.query (SProxy :: SProxy "") unit (Tuple <$> q <*> TCHE.get) >>= case _ of
+        Nothing -> throwError $ error "expanding component should have child"
+        Just (Tuple a new) -> a <$ H.put new
+  , render: \b ->
+    HH.slot (SProxy :: SProxy "") unit TCHE.expandingComponent (Tuple settings b)
+      (HE.input Up)
+  }
+
+type Slots =
+  ( expanding :: H.Slot QE String Zuruzuru.Key
+  )
+
 interactive :: H.Component HH.HTML Q Unit Void Aff
 interactive = H.component
   { initializer: Nothing
@@ -153,8 +207,12 @@ interactive = H.component
   , receiver: pure Nothing
   , initialState: pure { values: [], shown: true }
   , eval: case _ of
-      ZZ (Zuruzuru.NewState v) a ->
-        a <$ H.modify_ _ { values = v }
+      ZZ (Zuruzuru.NewState v) a -> a <$ H.modify_ _ { values = v } <* logShow v
+      ZZ (Zuruzuru.Added info) a -> a <$ do
+        logShow info
+        bindFlipped logShow $ H.query (SProxy :: SProxy "zuruzuru") unit $
+          Zuruzuru.queryInside (SProxy :: SProxy "expanding") info.key $
+            PassOn $ (TCHE.set (TCHE.Focused TC.empty) *> TCHE.get)
       ZZ _ a -> pure a
       Swap a -> a <$ H.modify_ \r -> r { shown = not r.shown }
   , render
@@ -166,66 +224,72 @@ interactive = H.component
       { values :: Array String, shown :: Boolean } ->
       H.ComponentHTML
         Q
-        ( zuruzuru :: Zuruzuru.SimpleSlot' Aff Unit String Unit )
+        ( zuruzuru :: Zuruzuru.Slot' Slots Aff Unit String Unit )
         Aff
     render { shown: false } =
-      HH.div [ HP.class_ $ H.ClassName "grid-container" ] $ pure $ HH.div
-        [ HP.class_ $ H.ClassName "only row" ]
-        [ HH.span_ []
-        , HH.text "{"
+      HH.div [ HP.class_ $ H.ClassName "ast" ]
+        [ HH.text "{"
         , btn (Just (Swap unit)) "more-horizontal" "feather inline active"
         , HH.text "}"
         ]
     render { values: vs } = HH.slot Zuruzuru._zuruzuru unit Zuruzuru.zuruzuru' (com vs) lifting
     btn :: forall q ps. Maybe (q Unit) -> String -> String -> H.ComponentHTML q ps Aff
-    btn q = btn'
-      [ HE.onClick (pure q), HP.disabled (isNothing q) ]
+    btn q = btn' [ HE.onClick (pure q), HP.disabled (isNothing q) ]
     btn' :: forall q ps. Array (HH.IProp HTMLbutton (q Unit)) -> String -> String -> H.ComponentHTML q ps Aff
     btn' p t c = HH.button p [ icon t [ class_ c ] ]
     add_btn :: forall ps. String -> String -> Zuruzuru.RenderAdder ps Aff
     add_btn t c q = Just $ btn (Just q) t c
 
-    com :: Array String -> Zuruzuru.SimpleInput' Aff Unit String
+    com :: Array String -> Zuruzuru.Input' Slots Aff Unit String
     com vs =
       { values: vs
       , direction: Zuruzuru.Vertical
       , default: pure mempty
-      , renderer: Zuruzuru.Renderer \datums { add, output } ->
-        HK.div [ HP.class_ $ H.ClassName "grid-container" ]
+      , renderer
+      }
+
+    renderer = Zuruzuru.Renderer \datums { add, output } ->
+      HH.div [ HP.class_ $ H.ClassName "ast" ] $ pure $
+      let postpend = add (Array.length datums) in
+      if Array.null datums
+      then HH.span_
+          [ HH.text "{"
+          , btn (Just postpend) "plus-square" "feather inline active"
+          , HH.text "}"
+          ]
+      else HK.div [ HP.class_ $ H.ClassName "grid-container" ]
         let
-          postpend = add (Array.length datums)
           items = datums <#> \{ helpers, handle, info } ->
             let
               moving :: forall r i. HP.IProp ( style :: String | r ) i
               moving = HP.attr (H.AttrName "style") $
                 "transform: translateY(" <> show info.offset <> "px)"
             in Tuple info.key $ HH.div [ HP.class_ $ H.ClassName "row" ]
-            [ if info.index == 0 then
-              btn (Just (output unit)) "minimize" "feather inline active"
-              else HH.span_ []
+            [ if info.index == 0
+              then btn (Just (output unit)) "minimize" "feather inline active"
+              else HH.span_ $ pure $ icon "more-vertical" [ class_ "feather inline passive" ]
             , HH.div_ [ HH.text if info.index == 0 then "{" else "," ]
-            , btn' [ moving, HP.ref handle.label, HE.onMouseDown handle.onMouseDown ] "menu" "feather inline active move vertical"
-            , HH.input
-              [ moving, HP.value info.value, HE.onValueInput (Just <<< helpers.set), HP.type_ InputText ]
-            , btn' [ moving, HE.onMouseDown (\_ -> Just helpers.remove) ] "minus-square" "feather inline active"
+            , btn'
+              [ moving
+              , HP.ref handle.label
+              , HE.onMouseDown handle.onMouseDown
+              , HP.class_ (H.ClassName "move")
+              , HP.tabIndex (-1)
+              ] "menu" "feather inline active move vertical"
+            , btn' [ moving, HE.onClick (\_ -> Just helpers.remove) ] "minus-square" "feather inline active"
+            , HH.span [ HP.class_ (H.ClassName "input-parent"), moving ]
+              [ HH.slot (SProxy :: SProxy "expanding") info.key
+                  expanding info.value (Just <<< helpers.set)
+              ]
             ]
         in items <>
-          [ Tuple "" if Array.null datums
-          then HH.div
-            [ HP.class_ $ H.ClassName "only row" ]
-            [ HH.span_ []
-            , HH.text "{"
-            , btn (Just postpend) "plus-square" "feather inline active"
-            , HH.text "}"
-            ]
-          else HH.div
+          [ Tuple "" $ HH.div
             [ HP.class_ $ H.ClassName "row" ]
             [ btn (Just (output unit)) "minimize" "feather inline active"
             , HH.text "}"
             , btn (Just postpend) "plus-square" "feather inline active"
             ]
           ]
-      }
 
 main :: Effect Unit
 main = HA.runHalogenAff do
