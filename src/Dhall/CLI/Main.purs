6a2c8e09c250233513cdf375e66c042906a3c868
Verity Scheel <monoidmusician@gmail.com>
2021-09-21 23:30:41 -0400

    Add command to test cache

--- src/Dhall/CLI/Main.purs
+++ src/Dhall/CLI/Main.purs
@@ -9,11 +9,16 @@ import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Newtype (unwrap, wrap)
 import Data.String as String
+import Data.Traversable (traverse)
 import Data.Tuple (Tuple(..), fst)
 import Data.Variant (Variant)
 import Dhall.Core as Dhall
+import Dhall.Core.CBOR (decode)
+import Dhall.Imports.Hash as Hash
+import Dhall.Imports.Resolve (ImportExpr)
 import Dhall.Imports.Resolve as Resolve
 import Dhall.Imports.Retrieve as Retrieve
+import Dhall.Lib.CBOR as CBOR
 import Dhall.Parser as Parser
 import Dhall.Printer as Printer
 import Dhall.TypeCheck as TC
@@ -24,10 +29,10 @@ import Effect.Class.Console (log, logShow)
 import Node.Process as Process
 import Validation.These as V
 
-runNormalize :: Array String -> Aff Unit
-runNormalize argv = case argv of
-  [ s ] -> normalizeFile s
-  _ -> log "Specify a single file to normalize"
+single :: forall a. String -> (a -> Aff Unit) -> Array a -> Aff Unit
+single thing fn argv = case argv of
+  [ s ] -> fn s
+  _ -> log ("Specify a single " <> thing)
 
 resolver :: Dhall.ImportType -> Resolve.R
 resolver target =
@@ -69,6 +74,19 @@ normalizeFile file = do
                 log $ print show normalized
                 pure unit
 
+deCBOR :: String -> Aff Unit
+deCBOR hash = do
+  cached <- Retrieve.nodeCache.get hash
+  case decode (CBOR.decode cached) of
+    Nothing -> logShow "decoding error"
+    Just decoded -> do
+      log $ print show decoded
+      case traverse (pure Nothing) decoded of
+        Nothing -> log "Unexpected imports"
+        Just noImports ->
+          when (hash /= Hash.hash ((absurd <$> Hash.neutralize noImports) :: ImportExpr)) do
+            log "Invalid hash after all"
+
 main :: Effect Unit
 main = do
   argv <- Array.drop 2 <$> liftEffect Process.argv
@@ -80,6 +98,7 @@ main = do
 
 commands :: Map String (Array String -> Aff Unit)
 commands = Map.fromFoldable
-  [ Tuple "normalize" runNormalize
+  [ Tuple "normalize" (single "file to normalize" normalizeFile)
   , Tuple "normalize-all" (traverse_ ((*>) <$> log <*> normalizeFile))
+  , Tuple "decode" (single "hash to decode from cache" deCBOR)
   ]
